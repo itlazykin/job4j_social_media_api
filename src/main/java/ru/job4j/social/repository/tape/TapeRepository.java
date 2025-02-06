7bcbe38870af37643252c36140466554e32aab67
itlazykin <dzaka89@gmail.com>
2025-02-06 10:35:44 +0300

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/social/repository/tape/TapeRepository.java
+++ src/main/java/ru/job4j/social/repository/tape/TapeRepository.java
@@ -1,7 +1,21 @@
 package ru.job4j.social.repository.tape;
 
-import org.springframework.data.repository.CrudRepository;
-import ru.job4j.social.model.Tape;
 
-public interface TapeRepository extends CrudRepository<Tape, Integer> {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.job4j.social.model.Tape;
+import ru.job4j.social.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TapeRepository extends JpaRepository<Tape, Integer> {
+
+    List<Tape> findByUser(User user);
+
+    List<Tape> findByCreatedPostGreaterThanEqualAndCreatedPostLessThanEqual(
+            LocalDateTime startTime, LocalDateTime endTime);
+
+    Page<Tape> findByOrderByCreatedPostDesc(Pageable pageable);
 }
