508438bd0e9711ec01dee261399e81eb817a22dd
itlazykin <dzaka89@gmail.com>
2025-02-06 12:29:08 +0300

    HQL, JPQL, Native SQL [#505064]

--- src/main/java/ru/job4j/social/repository/post/PostRepository.java
+++ src/main/java/ru/job4j/social/repository/post/PostRepository.java
@@ -1,7 +1,51 @@
 package ru.job4j.social.repository.post;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.social.model.Post;
 
-public interface PostRepository extends CrudRepository<Post, Integer> {
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = """
+            UPDATE posts
+            SET title = :title1, description = :description2
+            WHERE id = :id3
+            """, nativeQuery = true
+    )
+    int updateTitleAndDescriptionById(
+            @Param("title1") String title,
+            @Param("description2") String description,
+            @Param("id3") Long id
+    );
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = """
+            DELETE FROM posts
+            WHERE id = :id
+            """, nativeQuery = true
+    )
+    int deletePostById(@Param("id") Integer id);
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            value = """
+            DELETE FROM files
+            WHERE post_id = :id
+            """, nativeQuery = true
+    )
+    int deleteFileById(@Param("id") Integer id);
+
+    @Query(value = """
+            SELECT p FROM posts AS p
+            JOIN users AS u ON p.user_id = u.id
+            WHERE p.user_id IN :idUsers
+            ORDER BY p.id ASC
+            """, nativeQuery = true
+    )
+    List<Post> findByUserId(@Param("idUsers") List<Long> idUsers);
 }
--- src/main/java/ru/job4j/social/repository/user/UserRepository.java
+++ src/main/java/ru/job4j/social/repository/user/UserRepository.java
@@ -1,7 +1,49 @@
 package ru.job4j.social.repository.user;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.job4j.social.model.Post;
 import ru.job4j.social.model.User;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = """
+            FROM User
+            WHERE email = :email AND password = :password
+            """
+    )
+    Optional<User> findByEmailAndPassword(
+            @Param("email") String email,
+            @Param("password") String password
+    );
+
+    @Query(value = """
+            SELECT * FROM subscriptions
+            WHERE user_target_id = :id
+            """, nativeQuery = true
+    )
+    List<User> findByIdSubscriptions(@Param("id") Integer id);
+
+    @Query(value = """
+            SELECT * FROM friends
+            WHERE requester_id = :id
+            """, nativeQuery = true
+    )
+    List<User> findByIdFriends(@Param("id") Integer id);
+
+    @Query(value = """
+            SELECT p.user_id, p.title, p.description
+            FROM posts AS p
+            JOIN tapes AS t ON p.id = t.post_id
+            JOIN subscriptions AS s ON s.user_subscriber_id = p.user_id
+            WHERE s.user_target_id = :id
+            ORDER BY t.created_post DESC
+            """, nativeQuery = true
+    )
+    List<Post> findByIdPosts(@Param("id") Integer id, Pageable pageable);
 }
